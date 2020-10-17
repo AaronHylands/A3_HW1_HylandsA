c76bd9444d81f8bfddf69dda21f4766c844b93b9
AaronHylands <aaronhylands1@gmail.com>
2020-10-17 18:41:29 -0400

    Sucessfully got AJAX working and partially implemented

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@
     .then(res => res.json())
     .then(data => {
         console.log(data);
+        let profNameSlot = document.querySelector(".text-primary"),
+            classTimeSlot = document.querySelector(".list-inline"),
+            courseDescSlot = document.querySelector(".courseDescSlot"),
+            courseNameSlot = document.querySelector(".courseNameSlot");
+
+        profNameSlot.textContent = data.profname;
+        classTimeSlot.textContent = data.classtime;
+        courseDescSlot.textContent = data.coursedesc;
+        courseNameSlot.textContent = data.coursename;
+        
+
 
         // handleData
         // here's where you would call the function that puts the pieces on the page
